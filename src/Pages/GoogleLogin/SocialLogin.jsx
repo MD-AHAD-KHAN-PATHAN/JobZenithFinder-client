d91766917053a01c163e3194c75b6c86a3ec7165
MD-AHAD-KHAN-PATHAN <md.ahadkhanpathan.tct@gmail.com>
2023-11-05 13:15:01 +0600

    added registration page

---
+++ src/Pages/GoogleLogin/SocialLogin.jsx
@@ -0,0 +1,30 @@
+import { toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+import UseHooks from "../../Components/Hooks/UseHooks";
+
+const SocialLogin = () => {
+    const {googleLogin} = UseHooks();
+
+    function slow(){
+        Navigate(location?.state ? location.state : '/');
+    }
+
+    const handleLogin = () => {
+        googleLogin()
+        .then(() => {
+            toast.success('Successfully Login');
+            setTimeout(slow, 2000);
+        })
+        .then((error) => toast.error(error.message))
+    }
+    return (
+        <>
+            <div className="divider text-orange-500">continue with</div>
+            <div className="">
+                <button onClick={handleLogin} className="btn bg-white  hover:bg-orange-400 hover:text-white text-orange-500 border-1 border-orange-400">Google</button>
+            </div>
+        </>
+    );
+};
+
+export default SocialLogin;
